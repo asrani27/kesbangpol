3872ddb1a196fd070e210c816cd96984fcaa0387
asrani <asrani.27@gmail.com>
2021-05-25 21:44:49 +0800

    fix

--- resources/views/backend/dashboard_user.blade.php
+++ resources/views/backend/dashboard_user.blade.php
@@ -3,7 +3,7 @@
 @section('konten')
  <!-- Small boxes (Stat box) -->
     <div class="row">
-        <div class="col-lg-6 col-xs-6">
+        <div class="col-lg-12 col-xs-6">
           <!-- small box -->
           <div class="small-box bg-maroon">
             <div class="inner">
@@ -19,26 +19,12 @@
         </div>
         <!-- ./col -->
         
-        <div class="col-lg-6 col-xs-6">
-                <!-- small box -->
-                <div class="small-box bg-green">
-                  <div class="inner">
-                    <h3>1</h3>
-      
-                    <p>SPANDUK</p>
-                  </div>
-                  <div class="icon">
-                    <i class="fa fa-group"></i>
-                  </div>
-                  <a href="{{url('/ormas')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-                </div>
-        </div>
               <!-- ./col -->
     </div>
 
     <div class="row">
             <!-- Left col -->
-            <section class="col-lg-6 connectedSortable"> 
+            <section class="col-lg-12 connectedSortable"> 
                 <!-- Map box -->
                 <div class="box box-solid bg-green-gradient">
                   <div class="box-header">
@@ -53,24 +39,6 @@
                 <!-- /.box -->    
               </section>
             <!-- /.Left col -->
-
-
-            <!-- right col (We are only adding the ID to make the widgets sortable)-->
-            <section class="col-lg-6 connectedSortable"> 
-              <!-- Map box -->
-              <div class="box box-solid bg-light-blue-gradient">
-                <div class="box-header">
-                  <h3 class="box-title">
-                    Syarat Pendaftaran SPANDUK
-                  </h3>
-                </div>
-                <div class="box-body">
-                  
-                </div>
-              </div>
-              <!-- /.box -->    
-            </section>
-            <!-- right col -->
           </div>
 @endsection
 
