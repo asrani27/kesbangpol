c389b3eab4dedf285e046d5f4ba14d67559408ae
asrani <asrani.27@gmail.com>
2021-06-21 08:46:57 +0800

    fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,10 +5,11 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+                <div class="panel-heading"></div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
+                    <a href="/">Kembali</a>
+                    {{-- <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
@@ -60,7 +61,7 @@
                                 </a>
                             </div>
                         </div>
-                    </form>
+                    </form> --}}
                 </div>
             </div>
         </div>
