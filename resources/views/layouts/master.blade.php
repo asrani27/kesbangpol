1095d1e7a55211e416a1db1ddc84e5cb60637add
DESKTOP-Q4VBTMK\hanaf <hanafiahtkj@gmail.com>
2021-09-08 12:22:16 +0800

    #

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,6 +19,9 @@
   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
   <link rel="stylesheet" href="https://sso.banjarmasinkota.go.id/vendor/bjm-sso/bjm-sso.css">
 	<script src="https://sso.banjarmasinkota.go.id/vendor/bjm-sso/bjm-sso.js"></script>
+  <!-- <link rel="stylesheet"type="text/css" href="http://127.0.0.1:8000/vendor/bjm-sso/bjm-sso.css">
+	<script src="http://127.0.0.1:8000/vendor/bjm-sso/bjm-sso.js"></script> -->
+  
     @stack('add_css')
 
   <!-- Google Font -->
