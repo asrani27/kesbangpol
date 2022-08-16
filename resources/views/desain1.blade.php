b8b955f950357cf5917f697418f655e8f904ee1a
asrani <asrani.27@gmail.com>
2022-08-16 22:15:52 +0800

    f

--- resources/views/desain1.blade.php
+++ resources/views/desain1.blade.php
@@ -123,7 +123,7 @@
                     </div> <!-- container -->
                     <div class="slider-image-box d-none d-lg-flex align-items-end" style="height: 60%">
                         <div class="slider-image">
-                            &nbsp;&nbsp;&nbsp;&nbsp;<img src="/icon_kesbang/hut771.png" width="80%">
+                            &nbsp;&nbsp;&nbsp;&nbsp;<img src="/icon_kesbang/mp.png" width="80%">
                         </div> <!-- slider-imgae -->
                     </div> <!-- slider-imgae box -->
                 </div> <!-- carousel-item -->
