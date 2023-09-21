f6f35495ec0e67188b07fee4203a6e691be96069
asrani <asrani.27@gmail.com>
2023-09-21 13:54:36 +0800

    f

--- resources/views/backend/riset/printdata.blade.php
+++ resources/views/backend/riset/printdata.blade.php
@@ -315,8 +315,8 @@
 			&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<strong>Kepala Badan<br />
 				{{-- &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</strong>Sekretaris<br />
 
-			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; u.b<br />
-			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {{$map->data->jabatan}}<br /> --}}
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; u.b<br /> --}}
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {{$map->data->jabatan}}<br />
 			<br />
 			<br />
 			<br />
